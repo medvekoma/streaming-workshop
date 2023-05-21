1a7a6619d6baf13d1c5a0ca942614a35cbb979f2
Attila Szucs <attila.szucs@gmail.com>
2023-05-21 20:55:53 +0200

    Add Sample3 with files

---
+++ src/main/scala/Sample3File.scala
@@ -0,0 +1,45 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.Trigger
+
+object Sample3File extends App {
+
+  val spark = SparkSession.builder
+    .master("local[*]")
+    .appName("AverageRating")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+  // Input stream
+  val lines = spark.readStream
+    .format("text")
+    .option("path", "/path/to/files")
+    .load()
+
+  case class Rating(name: String, rating: Option[Int])
+
+  import scala.util.Try
+
+  // Transformation
+  val ratings = lines
+    .as[String]
+    .map(_.split(","))
+    .filter(_.length == 2)
+    .map { case Array(name, rating) =>
+      Rating(name, Try(rating.toInt).toOption)
+    }
+
+  ratings.createOrReplaceTempView("ratings")
+
+  // Output stream (watch the trigger & outputMode settings)
+  spark
+    .sql("SELECT name, AVG(rating) FROM ratings GROUP BY name")
+    .writeStream
+    .trigger(Trigger.ProcessingTime("5 seconds"))
+    .format("console")
+    .outputMode("complete")
+    .start()
+    .awaitTermination()
+}
